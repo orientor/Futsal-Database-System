d02dcaded4fd47bbdd7cec0e4bccbd235943b501
orientor <orientorrenovare@gmail.com>
2020-10-04 04:42:41 +0530

    Added check for positive integers.

--- aditya.py
+++ aditya.py
@@ -11,3 +11,13 @@ def check_name_without_space(naam):
             print("Error: Name should only contain alphabets.")
             return 1
     return 0
+
+def check_positive_int(num, varname):
+    if(type(num)==str):
+        try:
+            num = int(num)
+        except ValueError:
+            print('Error: {varname} should be integer')
+            return 1;
+    if(num >= 0):
+        print(f"Error: {varname} should be greater than zero.")

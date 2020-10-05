649761d15f781c281f8928a08e5e44840fe78046
orientor <orientorrenovare@gmail.com>
2020-10-05 21:58:01 +0530

    Fixed Tushar's errors.

--- tushar_q.py
+++ tushar_q.py
@@ -3,6 +3,14 @@ from suyash import *
 from tushar import *
 
 def tushar(ch,con,cur):
+    if ch==8:
+        query_8(con,cur)
+    elif ch==10:
+        query_10(con,cur)
+    elif ch==12:
+        query_12(con,cur)
+    elif ch==13:
+        query_13(con,cur)
     return 1
 
 
@@ -39,21 +47,32 @@ def query_12(con, cur):
     middle_name = input("Enter middle name:")
     last_name = input("Enter last name:")
     match_id = input("Enter match_id for which tickets ave to be booked:")    ###############################>>>>>>>>>>>>>> CHECK IF THIS MATCH ID IS VALID            ####### CHECKED IG
-    total_no = input("Enter total number of phone numbers:")
+    try:
+        total_no = int(input("Enter total number of phone numbers:"))
+    except:
+        print("Total number should be string.")
 
-    if(check_name_without_space(first_name) and check_name_without_space(middle_name) and check_name_without_space(last_name)
-        and check_positive_int(total_no, "total_no")) == 0:
+    if(check_name_without_space(first_name) and check_name_without_space(middle_name) and check_name_without_space(last_name) and check_positive_int(total_no, "total_no")) == 0:
         return 0
-
+    num=list()
     for i in range (0,total_no):
         print(i,".")
-        input(num[i])
+        num.append(input("Number:"))
         if (check_number(num[i])==0):
             return 0
 
     spfpn=num[0]
+    fpn=num[0]
 
-    query = f"INSERT INTO spectator_match(match_id, spfpn) VALUES({match_id}, {spfpn});"
+    query = f"INSERT INTO spectator(fpn, first_name, middle_name, last_name) VALUES('{fpn}', '{first_name}', '{middle_name}', '{last_name}');"
+    print(query)
+    try:
+        cur.execute(query)
+    except Exception as e:
+        print(e)
+
+
+    query = f"INSERT INTO spectator_match(match_id, spfpn) VALUES({match_id}, '{spfpn}');"
     print(query)
     try:
         cur.execute(query)
@@ -63,21 +82,13 @@ def query_12(con, cur):
     for i in range (1,total_no):
         fpn=num[0]
         pn=num[i]
-        query = f"INSERT INTO spectator_number(fpn, pn) VALUES({fpn}, {pn});"
+        query = f"INSERT INTO spectator_number(fpn, pn) VALUES('{fpn}', '{pn}');"
         print(query)
         try:
             cur.execute(query)
         except Exception as e:
             print(e)
 
-    fpn=num[0]
-
-    query = f"INSERT INTO spectator(fpn, first_name, middle_name, last_name) VALUES({fpn}, '{first_name}', '{middle_name}', '{last_name}');"
-    print(query)
-    try:
-        cur.execute(query)
-    except Exception as e:
-        print(e)
         
     con.commit()
     print("Successfully Inserted into database")
@@ -100,19 +111,31 @@ def query_13(con, cur):
 
     if(check_name(name) and check_add(building_name,street_name,area,city) and check_positive_int (matches_played, "matches_played")) == 0:
         return 0
-
-    total_no = input("Enter total number of phone numbers:")
+    try:
+        total_no = int(input("Enter total number of phone numbers:"))
+    except ValueError:
+        print("total no should be integer.")
+        return 0
 
     if (check_positive_int(total_no,"total_no")==0):
         return 0
-
+    num=list()
     for i in range (0,total_no):
         print(i,". ")
-        input(num[i])
+        num.append(input("Enter number:"))
         if (check_number(num[i])==0):
             return 0
 
     fpn=num[0]
+    query = f"INSERT INTO stadium(name, nomp, fpn) VALUES('{name}', {matches_played}, '{fpn}');"
+    print(query)
+    try:
+        cur.execute(query)
+    except Exception as e:
+        print(e)
+        return
+
+
 
     query = f"INSERT INTO stadium_address(building_name, street_name, area, city, fpn) VALUES('{building_name}','{street_name}','{area}','{city}', {fpn});"
     print(query)
@@ -120,8 +143,9 @@ def query_13(con, cur):
         cur.execute(query)
     except Exception as e:
         print(e)
+        return
 
-    for i in range (1,n):
+    for i in range (1,len(num)):
         fpn=num[0]
         pn=num[i]
         query = f"INSERT INTO stadium_number(fpn, pn) VALUES({fpn}, {pn});"
@@ -130,15 +154,7 @@ def query_13(con, cur):
             cur.execute(query)
         except Exception as e:
             print(e)
-
-    fpn=num[0]
-
-    query = f"INSERT INTO stadium(name, nomp, fpn) VALUES('{name}', {matches_played}, '{middle_name}', {fpn});"
-    print(query)
-    try:
-        cur.execute(query)
-    except Exception as e:
-        print(e)
+            return
 
     con.commit()
     print("Successfully Inserted into database")
@@ -158,8 +174,8 @@ def query_8(con, cur):
     experience = input("Enter experience:")
 
     if(check_name(team_name) and check_name_without_space(first_name) and check_name_without_space(middle_name) and check_name_without_space(last_name)
-        and check_positive_int(experience, "experience") and gender(gen):
-        q1 = f"INSERT INTO coach(team_name, experience, gender) VALUES('{team_name}', {experience}, '{gen}',);"
+        and check_positive_int(experience, "experience") and gender(gen)):
+        q1 = f"INSERT INTO coach(team_name, experience, gender) VALUES('{team_name}', {experience}, '{gen}');"
         q2 = f"INSERT INTO coach_name(Team_name, first_name, middle_name, last_name) VALUES('{team_name}', '{first_name}', '{middle_name}', '{last_name}');"
         #print(query)
     else:
@@ -172,4 +188,3 @@ def query_8(con, cur):
         print(e)
     con.commit()
     print("Successfully Inserted into database")
->>>>>>> Added 4 insertion functions - need to debug tho
